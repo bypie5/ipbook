d766515210d3c00f1324a182bfab792540d18918
bypie5 <bypie966@gmail.com>
2018-12-21 23:21:55 -0800

    Added functionality of ipbook

--- ipbook/__main__.py
+++ ipbook/__main__.py
@@ -1,21 +1,50 @@
-import os
-import argparse
+import os, argparse, json
+
+dir_path, file_name = os.path.split(os.path.abspath(__file__))
+data_path = os.path.join(dir_path, 'data/ip_addrs.json')
+
+def loc_fname(ips, fname):
+	for i, entry in enumerate(ips):
+		if entry["name"] == fname:
+			return i
+	return -1
 
 def add_ip(args):
-	print (args.ip + ', ' + args.fname)
+	with open(data_path, "r+") as f:
+		data = json.load(f)
+		if loc_fname(data["ips"], args.fname) == -1:
+			f.seek(0)
+			data["ips"].append({"ip": args.ip, "name": args.fname})
+			json.dump(data, f)
+			print("Added " + args.fname)
+		else:
+			print("Cannot add duplicate name!")
 
 def list_ips(args):
-	print ('hello buckaroo')
-
+	with open(data_path) as f:
+		data = json.load(f)
+	
+	for i, entry in enumerate(data["ips"]):
+		print(str(i+1) + ") " + data["ips"][i]["name"] + ": " + data["ips"][i]["ip"])
+	
 def delete_ip(args):
-	print ('Deleting ' + args.fname + '...')
+	with open(data_path) as f:
+		data = json.load(f)
+		f.seek(0)
+		
+	i = loc_fname(data["ips"], args.fname)
+	if not i == -1:
+		del data["ips"][i]
+		with open(data_path, "w+") as o:	
+			json.dump(data, o)
+
+		print ("Deleted " + args.fname)
+	else:
+		print(args.fname + " not in records")
 
 def main():
-	'''
-	dir_path, file_name = os.path.split(os.path.abspath(__file__))
-	data_path = os.path.join(dir_path, 'data/ip_addrs.json')
-	'''
-	parser = argparse.ArgumentParser(description="Save a list of your most commonly used IP addresses")
+	parser = argparse.ArgumentParser(description=
+		"Save a list of your most commonly used IP addresses")
 	subparser = parser.add_subparsers()
 
 	# Add command
@@ -29,7 +58,8 @@ def main():
 	list_parser.set_defaults(func=list_ips)
 
 	# Delete command
-	delete_parser = subparser.add_parser('remove', help="Remove IP record by friendly name")
+	delete_parser = subparser.add_parser('remove', help=
+		"Remove IP record by friendly name")
 	delete_parser.add_argument('fname', help="Record's friendly name")
 	delete_parser.set_defaults(func=delete_ip)
 
